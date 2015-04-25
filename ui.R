b1f31aa9dd175e3324e99805ffc327e0a6dec05b
GauiGud <gaui@mf.is>
2015-04-25 18:41:01 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -17,7 +17,11 @@ shinyUI(pageWithSidebar(
         
         # Use Slider to choose how many tosses / Rolls
         sliderInput('rolls', 'How many tosses/rolls', 
-                    value = 10, min = 10, max = 1000, step = 10,)
+                    value = 10, min = 10, max = 1000, step = 10,),
+
+        # Create links to ui.R and Server.R on GitHub
+        helpText(HTML("Here is a link to the <a href = \"https://github.com/GauiGud/DevelopingDataProducts/blob/master/ui.R\">ui.R file</a>")),
+        helpText(HTML("Here is a link to the <a href = \"https://github.com/GauiGud/DevelopingDataProducts/blob/master/Server.R\">Server.R file</a>"))        
     ),
     mainPanel(
         plotOutput('newPlot')
