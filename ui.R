9dbfa067ab61e7a0142e58cfc4a3947639b53805
GauiGud <gaui@mf.is>
2015-04-25 18:17:51 +0000

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,25 @@
+text <- paste("This is a simulation of Coin tosses and Die rollings.  ",
+              "You can choose how many tosses/rolls you want by using the slider.",
+              "The graph shows the results of the simulation along with a red line ",
+              "which idicates the expected value.")
+
+shinyUI(pageWithSidebar(
+    headerPanel("Coin tosses / Die rolling simulation"),
+
+    sidebarPanel(
+        
+        p(text),
+        
+        # Use RadioButtons to choose between a Coin and Die
+        radioButtons("type", "Choose Coin or Die simulation:",
+                     c("Coin toss" = "coin",
+                       "Die roll" = "die")),
+        
+        # Use Slider to choose how many tosses / Rolls
+        sliderInput('rolls', 'How many tosses/rolls', 
+                    value = 10, min = 10, max = 1000, step = 10,)
+    ),
+    mainPanel(
+        plotOutput('newPlot')
+    )
+))
